b4bf7e2757d78ba32052f5e8a4a0f80c96115e40
alexey <aleksejmolokanov72908@gmail.com>
2025-03-12 21:10:04 +0300

    added test todoList

--- internal/service/todo_list_test.go
+++ internal/service/todo_list_test.go
@@ -80,3 +80,98 @@ func TestTodoListService_Failure(t *testing.T) {
 	assert.Empty(t, result)
 
 }
+
+func TestTodoListService_GetById_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+
+	userId := 1
+	listId := 1
+	list := models.TodoList{
+		Id:          1,
+		Title:       "test",
+		Description: "test",
+	}
+	mockRepo.EXPECT().GetById(userId, listId).Return(list, nil).Times(1)
+	result, err := listService.GetById(userId, listId)
+	assert.NoError(t, err)
+	assert.Equal(t, list, result)
+
+}
+
+func TestTodoListService_GetById_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+
+	userId := 1
+	listId := 1
+
+	mockRepo.EXPECT().GetById(userId, listId).Return(models.TodoList{}, errors.New("error")).Times(1)
+	result, err := listService.GetById(userId, listId)
+	assert.Error(t, err)
+	assert.Empty(t, result)
+}
+
+func TestTodoListService_Delete_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+	userId := 1
+	listId := 1
+	mockRepo.EXPECT().Delete(userId, listId).Return(nil).Times(1)
+	err := listService.Delete(userId, listId)
+	assert.NoError(t, err)
+}
+
+func TestTodoListService_Delete_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+	userId := 1
+	listId := 1
+	mockRepo.EXPECT().Delete(userId, listId).Return(errors.New("error")).Times(1)
+	err := listService.Delete(userId, listId)
+	assert.Error(t, err)
+}
+
+func TestTodoListService_Update_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+	userId := 1
+	listId := 1
+	title := "test"
+	description := "test"
+	input := models.UpdateListInput{
+		Title:       &title,
+		Description: &description,
+	}
+	mockRepo.EXPECT().Update(userId, listId, input).Return(nil).Times(1)
+	err := listService.Update(userId, listId, input)
+	assert.NoError(t, err)
+}
+
+func TestTodoListService_Update_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockTodoList(ctrl)
+	listService := NewTodoListService(mockRepo)
+	userId := 1
+	listId := 1
+	title := "test"
+	description := "test"
+	input := models.UpdateListInput{
+		Title:       &title,
+		Description: &description,
+	}
+	mockRepo.EXPECT().Update(userId, listId, input).Return(errors.New("error"))
+	err := listService.Update(userId, listId, input)
+	assert.Error(t, err)
+}
